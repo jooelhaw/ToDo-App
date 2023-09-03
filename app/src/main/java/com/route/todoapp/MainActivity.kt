02c8769f22eeb1938f57c2f295c2709084a03dbb
joo <jooelhaw@gmail.com>
2023-09-03 18:49:10 +0300

    base UI + Init db 3

--- app/src/main/java/com/route/todoapp/MainActivity.kt
+++ app/src/main/java/com/route/todoapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.route.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.marginBottom
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.route.todoapp.databinding.ActivityMainBinding
+import com.route.todoapp.databinding.HomeContentBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewBinding: ActivityMainBinding
